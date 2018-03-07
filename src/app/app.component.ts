94423a909a5f2b03980a0ec14fb241060724db02
kiluakun <kiluakun@gmail.com>
2018-03-07 10:16:33 -0300

    Carga inicial de codigo
    Proyecto corresponde al primer proyecto de Udemy/Angular de 0 a experto.

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hola Mundo!!';
+  nombre:string = 'Sebastian';
+  apellido:string = 'Segovia';
+}
